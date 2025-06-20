2d0ca8910c72dee166959b63811a61c840ea1cf6
Pushpendrabaghel45 <bpushpendra501@gmail.com>
2025-06-20 13:32:41 +0530

    Add files via upload

---
+++ connect1.php
@@ -0,0 +1,27 @@
+<?php
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$mail = $_POST['mail'];
+$phone = $_POST['phone'];
+$message =$_POST['message'];
+
+
+// Database connection
+
+$conn = new mysqli('localhost','root','','pp');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+ $stmt = $conn->prepare("Insert into Form1 (firstName, lastName, mail, phone, message) values (?,?,?,?,?)");
+
+ $stmt->bind_param ("sssis", $firstname, $lastname, $mail, $phone, $message);
+
+ $execval = $stmt -> execute ();
+ echo $execval;
+ echo "Registration successfully.......";
+
+ $stmt->close();
+ $conn->close();
+}
+ ?>
