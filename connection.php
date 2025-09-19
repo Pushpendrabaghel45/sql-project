9def630690d5f4e9dd575d31f7497ffa1d22220f
Pushpendra baghel <bpushpendra501@gmail.com>
2025-09-19 12:15:01 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -6,9 +6,11 @@ $database = 'login_register_db';
 
 if (isset($_POST))
 
+    
     $conn = new mysqli($server, $username, $password, $database);
 if ($conn) {
     // echo 'Server Connected Success';
 } else {
     die(mysqli_error($conn));
 }
+
